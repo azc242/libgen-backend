2e7acf198f779d5dccff8eb928b79429d51fd404
Alan Chen <azc242@nyu.edu>
2020-07-16 22:06:03 -0700

    clean up code comments

--- app.js
+++ app.js
@@ -24,16 +24,7 @@ app.get('/atomic', function(req, res){
 
       const data = await libgen.search(options)
       let n = data.length;
-      // console.log('top ' + n + ' results for "' +
-      //             options.query + '"');
       while (n--){
-        // console.log('***********');
-        // console.log('Title: ' + data[n].title);
-        // console.log('Author: ' + data[n].author);
-        // console.log('Download: ' +
-        //             'http://gen.lib.rus.ec/book/index.php?md5=' +
-        //             data[n].md5.toLowerCase());
-
         const searchResult = {
           title: data[n].title,
           author: data[n].author,
